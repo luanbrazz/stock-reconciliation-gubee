906b794607df5538fcb102dc79d2e38a2613128d
Luan Braz <luan_silvacpv@hotmail.com>
2026-05-29 15:31:00 -0300

    chore: initial project setup via Spring Initializr

---
+++ stock-reconciliation-api/src/main/java/com/gubee/stock/StockReconciliationApiApplication.java
@@ -0,0 +1,13 @@
+package com.gubee.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockReconciliationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockReconciliationApiApplication.class, args);
+	}
+
+}
